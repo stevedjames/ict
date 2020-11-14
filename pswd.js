d5d317eb4d4a3fb3a4c81d5fdd1ef769be89aac2
stevedjames <64777330+stevedjames@users.noreply.github.com>
2020-11-14 16:48:57 +0530

    Add files via upload

--- pswd.js
+++ pswd.js
@@ -13,16 +13,35 @@ tex.innerHTML="";
 password.addEventListener('input', function() {
     var val = password.value;
     var result = zxcvbn(val);
-  
+    var strongRegex = new RegExp("^(?=.{8,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*$", "g");
+    var mediumRegex = new RegExp("^(?=.{6,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$", "g");
+   
     // Update the password strength meter
-    meter.value = result.score;
-  
-    // Update the text indicator
-    if (val !== "") {
-      text.innerHTML = "Strength: " + strength[result.score]; 
-    } else {
-      text.innerHTML = "";
-    }
+    if(strongRegex.test(password.value)){
+      text.innerHTML="Strong";
+      
+      meter.value=4;
+      
+  }else if(password.value.length<=5){
+      text.innerHTML="Weak!!!";
+      
+      meter.value=1;
+      
+  }else if(password.value.length==0){
+    meter.value=0;
+  }
+  else if(mediumRegex.test(password.value)){
+      text.innerHTML="Medium";
+     
+      meter.value=2;
+     
+  }else{
+     
+    
+      meter.value=0;
+      
+  }
+
   });
 function pswd(){
     
